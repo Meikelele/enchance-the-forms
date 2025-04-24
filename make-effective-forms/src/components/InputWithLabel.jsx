6cedf1e723bda0c7e4bcef0e35c8d892e6ed24d8
Meikelele <michal.bak2002@interia.pl>
2025-04-24 15:39:54 +0200

    styling InputWithLabel

--- make-effective-forms/src/components/InputWithLabel.jsx
+++ make-effective-forms/src/components/InputWithLabel.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const InputWithLabel = ({id, label, type = 'text', required = false, placeholder, value, onChange}) => {
     return (
         <div className='mb-3'>
-            <label htmlFor={id} className='form-label'>{label}</label>
+            <label htmlFor={id} className='form-label' style={{marginBottom: '0rem'}}>{label}</label>
             <input id={id} className='form-control' type={type} required={required} placeholder={placeholder} value={value} onChange={onChange} />
         </div>
     )
