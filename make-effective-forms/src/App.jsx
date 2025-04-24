6ce53cf8a55db42d31b5f55e55fa11c5c3891fad
Meikelele <michal.bak2002@interia.pl>
2025-04-24 13:59:30 +0200

    added FormContainer to App

--- make-effective-forms/src/App.jsx
+++ make-effective-forms/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import FormContainer from './components/FormContainer'
 
 function App() {
   return (
-    <div className="container">
-      <h1>Efektywny formularz-container</h1>
+    <div className="container py5">
+      <h1 className='mb-5'>Efektywny formularz !!!</h1>
+      <FormContainer />
     </div>
   )
 }
